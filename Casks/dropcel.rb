cc5f003f1210ebb1733ce8723f6c3a56267895f6
Diego Peralta <me@diego.earth>
2026-07-23 20:01:03 +0200

    dropcel 0.1.0: pin sha256

--- Casks/dropcel.rb
+++ Casks/dropcel.rb
@@ -1,6 +1,6 @@
 cask "dropcel" do
   version "0.1.0"
-  sha256 :no_check
+  sha256 "47e1f5c5234753b4fccfdd2c7103cf8c69e4b803f13bdc951731f938581fce3e"
 
   url "https://github.com/jagenaujagenau/dropcel/releases/download/v#{version}/Dropcel_#{version}_universal.dmg"
   name "Dropcel"
